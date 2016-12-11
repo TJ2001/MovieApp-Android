ba9d239d0438cc74913c4f369d76e4cb9a10a73a
TJ2001 <jung.m.tim@gmail.com>
2016-12-10 21:14:56 -0800

    fix gradle errors

--- app/src/main/java/com/example/guest/movieapp/Constants.java
+++ app/src/main/java/com/example/guest/movieapp/Constants.java
@@ -9,7 +9,6 @@ public class Constants {
     public static final String LANGUAGE = "language";
     public static final String INCLUDE_ADULT = "include_adult";
     public static final String TMDB_API_KEY = BuildConfig.MTDB_API_KEY;
-    public static final String TMDB_TOKEN = BuildConfig.MTDB_TOKEN;
     public static final String API_KEY = "api_key";
     public static final String imageURL = "https://image.tmdb.org/t/p/w300_and_h450_bestv2";
 
